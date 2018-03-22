c7a20c74cdece8ec9f7df024e3ea24ffbb7f6aaf
hasan330 <hasan.saleh.khader@gmail.com>
2018-03-22 16:58:19 +0200

    Inheritance between Vehicle and Car + initial setup for refuel class

---
+++ src/com/hasan/FuelConsumption.java
@@ -0,0 +1,16 @@
+package com.hasan;
+
+import java.util.Date;
+
+public class FuelConsumption {
+    private Date refuelDate;
+    private Long initialMillage;
+    private int cost;
+    private double litresFilled;
+    private int proposedDistance;
+    private int actualDistance;
+
+    private longDistances longDistance;
+
+
+}
