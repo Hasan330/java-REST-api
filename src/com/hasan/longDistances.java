6cf0eab4029d81193b9c5675579ed816d50498e2
hasan330 <hasan.saleh.khader@gmail.com>
2018-03-25 08:03:13 +0200

    Fix constructor calling + add longDistances class

---
+++ src/com/hasan/longDistances.java
@@ -0,0 +1,11 @@
+package com.hasan;
+
+import java.util.Date;
+
+public class longDistances {
+    private String source;
+    private String destination;
+    private int distance;
+    private Date dateOfTrip;
+
+}
