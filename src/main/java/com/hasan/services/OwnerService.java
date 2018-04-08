90b88b699d1304cd5d989aa48596e40694de2aec
hasan330 <hasan.saleh.khader@gmail.com>
2018-04-08 14:09:34 +0300

    add transactional annotation

--- src/main/java/com/hasan/services/OwnerService.java
+++ src/main/java/com/hasan/services/OwnerService.java
@@ -1,4 +1,38 @@
 package com.hasan.services;
 
+import com.hasan.models.Owner;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
 public class OwnerService implements OwnerServiceImpl {
+
+
+    @Override
+    @Transactional
+    public List<Owner> getOwners() {
+        return null;
+    }
+
+    @Override
+    @Transactional
+    public void saveOwner(Owner theOwner) {
+
+    }
+
+    @Override
+    @Transactional
+    public Owner getOwner(int theId) {
+        return null;
+    }
+
+    @Override
+    @Transactional
+    public void deleteOwner(int theId) {
+
+    }
+
+
 }
--- src/main/java/com/hasan/services/OwnerServiceImpl.java
+++ src/main/java/com/hasan/services/OwnerServiceImpl.java
@@ -1,4 +1,17 @@
 package com.hasan.services;
 
+import com.hasan.models.Owner;
+
+import java.util.List;
+
 public interface OwnerServiceImpl {
+
+    public List<Owner> getOwners();
+
+    public void saveOwner(Owner theOwner);
+
+    public Owner getOwner(int theId);
+
+    public void deleteOwner(int theId);
+
 }
