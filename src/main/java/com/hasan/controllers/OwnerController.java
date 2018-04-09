a06ff73759895fdf3965526803a3cb8451bfcb48
hasan330 <hasan.saleh.khader@gmail.com>
2018-04-09 12:04:04 +0300

    Multiple ways to /get

--- src/main/java/com/hasan/controllers/OwnerController.java
+++ src/main/java/com/hasan/controllers/OwnerController.java
@@ -31,8 +31,18 @@ public class OwnerController {
     }
 
 
+
+    //TODO: Check which of the below implementations is better (Path variable vs Request param)
+    @GetMapping("/get/{id}")
+    public Owner getOwnerById(@PathVariable int id){
+        System.out.println("Hit the get in owner for owner with id " + id);
+        Owner owner = ownerService.getOwner(id);
+        return owner;
+    }
+
+
     @GetMapping("/get")
-    public Owner getOwner(@RequestParam(value="id", defaultValue="2") int id){
+    public Owner getOwner(@RequestParam(value="id") int id){
         System.out.println("Hit the get in owner");
         Owner owner = ownerService.getOwner(id);
         return owner;
