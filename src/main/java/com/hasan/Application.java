4ec23c599c62c17989b27c62a4ab5cd959e9db51
hasan330 <hasan.saleh.khader@gmail.com>
2018-05-03 09:18:14 +0300

    Cleaning up + Changing connection pool sizE

--- src/main/java/com/hasan/Application.java
+++ src/main/java/com/hasan/Application.java
@@ -10,15 +10,5 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-
-
-//        RestTemplate restTemplate = new RestTemplate();
-//        Quote quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
-//        System.out.println("Quote is: " + quote.toString());
-
-        RestTemplate restTemplate = new RestTemplate();
-//        Owner owner = restTemplate.getForObject("https://randomuser.me/api/", Owner.class);
-        Owner owner = restTemplate.getForObject("https://qhc7ac9d7c.execute-api.us-east-1.amazonaws.com/development/sampleFunction", Owner.class);
-        System.out.println("Quote is: " + owner.toString());
     }
 }
